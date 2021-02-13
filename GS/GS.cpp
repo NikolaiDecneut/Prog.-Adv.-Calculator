e4b2da9378aee4cb5ac7894417cd1806017d16b2
Nikolai Decneut <decneutn@rsd407.org>
2021-02-12 17:02:07 -0800

    Add project files.

---
+++ GS/GS.cpp
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+#include <iostream>
+#include <conio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string>
+#include <string>
+
+int main()
+{
+    // variables below
+
+
+
+
+
+    // code start below
+
+
+
+
+
+
+    return 0;
+}
+
+
