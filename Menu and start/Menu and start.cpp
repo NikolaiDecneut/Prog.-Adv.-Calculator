d135f3cb307b8b3157da3307aa0632c8cfb1f396
Nikolai Decneut <decneutn@rsd407.org>
2021-06-08 22:08:37 -0700

    menu mhm

--- Menu and start/Menu and start.cpp
+++ Menu and start/Menu and start.cpp
@@ -4,10 +4,26 @@
 int main()
 {
 	// here will be the menu
-	printf("welcome!");
-	int salesTax;
+	printf("welcome to quick calc!\n");
+	int salesTax = .065;
+	int salesEnd;
+	int sales;
 	int pythagTheorom;
-
+	int equations;
+	// int  ;
+	// int  ;
+	printf("please select your type of equation\n");
+	switch (equations)
+	{
+	case 1:
+		printf("the sales taxs in WA is 6.5% so what is the price of the item you would like to calculate?\n");
+		
+		scanf_s("%d", sales);
+		fseek(stdin, 0, SEEK_END);
+		salesEnd == sales * salesTax;
+		printf("Your answeris: %d", salesEnd);
+		break;
+	}
 
 
 
