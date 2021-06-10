1a09cdcaf034827abec7931004ea9876bfea769d
Nikolai Decneut <decneutn@rsd407.org>
2021-06-10 08:06:27 -0700

    getting closer to being done

--- Menu and start/Menu and start.cpp
+++ Menu and start/Menu and start.cpp
@@ -8,8 +8,8 @@ int main()
 	float salesTax = .065;
 	int salesEnd;
 	int sales;
-	float citytax;
-	int pythagTheorom;
+	float cityTax;
+	//int pythagTheorom;
 	int equations;
 	
 	printf("Please select your type of equation\n 1. Sales tax by city\n");
@@ -23,13 +23,18 @@ int main()
 		printf("1. Duvall\n2. Bellevue\n3.Seattle");
 		scanf_s("%d", &city);
 		fseek(stdin, 0, SEEK_END);
+		printf("%d\n", city);
 		if (city == 1)
 		{
-			printf("Duvall city tax is 2.4% which makes it .024");
-			citytax = .024;
+			printf("Duvall city tax is 2.4%% which makes it .024");
+			cityTax = .024;
 		}
-			
-			
+		else if (city == 2)
+		{
+			printf("Bellevue city tax is 3.6%% which makes it .036");
+			cityTax = .036;
+		}
+		printf("Please defy the price of the item you purchased");
 		scanf_s("%d", &sales);
 		fseek(stdin, 0, SEEK_END);
 		salesEnd = sales * salesTax;
