0c3c45a442d5b2fa09ba12fd3eac740bf6f6652e
Nikolai Decneut <decneutn@rsd407.org>
2021-06-10 07:46:35 -0700

    update

--- Menu and start/Menu and start.cpp
+++ Menu and start/Menu and start.cpp
@@ -5,23 +5,35 @@ int main()
 {
 	// here will be the menu
 	printf("welcome to quick calc!\n");
-	int salesTax = .065;
+	float salesTax = .065;
 	int salesEnd;
 	int sales;
+	float citytax;
 	int pythagTheorom;
 	int equations;
-	// int  ;
-	// int  ;
-	printf("Please select your type of equation\n");
+	
+	printf("Please select your type of equation\n 1. Sales tax by city\n");
+	scanf_s("%d", &equations);
+	fseek(stdin, 0, SEEK_END);
 	switch (equations)
 	{
 	case 1:
-		printf("The sales taxs in WA is 6.5% so what is the price of the item you would like to calculate?\n");
-		
-		scanf_s("%d", sales);
+		int city;
+		printf("The sales taxs in WA is 6.5%, but each city has it own taxes so please select your city first\n");
+		printf("1. Duvall\n2. Bellevue\n3.Seattle");
+		scanf_s("%d", &city);
 		fseek(stdin, 0, SEEK_END);
-		salesEnd == sales * salesTax;
-		printf("Your answeris: %d", salesEnd);
+		if (city == 1)
+		{
+			printf("Duvall city tax is 2.4% which makes it .024");
+			citytax = .024;
+		}
+			
+			
+		scanf_s("%d", &sales);
+		fseek(stdin, 0, SEEK_END);
+		salesEnd = sales * salesTax;
+		printf("Your answer is: %d", salesEnd);
 		break;
 	}
 
