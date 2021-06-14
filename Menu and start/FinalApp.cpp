88f1fa7f6cc2d50f523f787d564141721ccdd330
Nikolai Decneut <decneutn@rsd407.org>
2021-06-14 08:37:54 -0700

    got it to work again

--- Menu and start/FinalApp.cpp
+++ Menu and start/FinalApp.cpp
@@ -7,7 +7,7 @@ int main()
 	printf("welcome to quick calc!\n");
 	float salesTax = .065;
 	int salesEnd;
-	int sales;
+	float sales;
 	float cityTax;
 	//int pythagTheorom;
 	int equations;
@@ -20,7 +20,7 @@ int main()
 	case 1:
 		int city;
 		printf("The sales taxs in WA is 6.5%, but each city has it own taxes so please select your city first\n");
-		printf("1. Duvall\n2. Bellevue\n3.Seattle");
+		printf("1. Duvall\n2. Bellevue\n3.Seattle\n");
 		scanf_s("%d", &city);
 		fseek(stdin, 0, SEEK_END);
 		printf("%d\n", city);
@@ -28,17 +28,23 @@ int main()
 		{
 			printf("Duvall city tax is 2.4%% which makes it .024");
 			cityTax = .024;
+			printf("Please defy the price of the item you purchased\n");
+			scanf_s("%f", &sales);
+			fseek(stdin, 0, SEEK_END);
+			salesEnd = sales * salesTax;
+			printf("Your answer is: %d USD", salesEnd);
 		}
 		else if (city == 2)
 		{
-			printf("Bellevue city tax is 3.6%% which makes it .036");
+			printf("Bellevue city tax is 3.6%% which makes it .036\n");
 			cityTax = .036;
+			printf("Please defy the price of the item you purchased\n");
+			scanf_s("%f", &sales);
+			fseek(stdin, 0, SEEK_END);
+			salesEnd = sales * salesTax;
+			printf("Your answer is: %d USD", salesEnd);
 		}
-		printf("Please defy the price of the item you purchased");
-		scanf_s("%d", &sales);
-		fseek(stdin, 0, SEEK_END);
-		salesEnd = sales * salesTax;
-		printf("Your answer is: %d", salesEnd);
+		
 		break;
 	}
 
