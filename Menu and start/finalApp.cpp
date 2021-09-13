9c3b7a0ceca5c7747d0d37b55c727d6a146b2566
NikolaiDecneut <decneutn@rsd407.org>
2021-09-13 09:35:25 -0700

    yuriyi

--- Menu and start/finalApp.cpp
+++ Menu and start/finalApp.cpp
@@ -15,7 +15,7 @@ int main()
 	float cityTax;
 	//int pythagTheorom;
 	int equations;
-	
+	//change ints to floats
 	printf("Please select your type of equation\n 1. Sales tax by city\n");
 	scanf_s("%d", &equations);
 	fseek(stdin, 0, SEEK_END);
@@ -24,7 +24,7 @@ int main()
 	case 1:
 		int city;
 		printf("The sales taxs in WA is 6.5%, but each city has it own taxes so please select your city first\n");
-		printf("1. Duvall\n2. Bellevue\n3.Seattle\n");
+		printf("1.Duvall\n2.Bellevue\n3.Seattle\n4.Spokane");
 		scanf_s("%d", &city);
 		fseek(stdin, 0, SEEK_END);
 		printf("%d\n", city);
@@ -42,7 +42,7 @@ int main()
 		{
 			printf("Bellevue city tax is 3.6%% which makes it .036\n");
 			cityTax = .036;
-			printf("Please defy the price of the item you purchased\n");
+			printf("Please defy the price of the item you purchased: \n");
 			scanf_s("%f", &sales);
 			fseek(stdin, 0, SEEK_END);
 			salesEnd = sales * salesTax;
@@ -50,9 +50,21 @@ int main()
 		}
 		else if (city == 3)
 		{
-			printf("Seattle city tax is  which makes the tax  \n");
+			printf("Seattle city tax is 6.5% which makes the tax .065 \n");
+			cityTax = .065;
+			printf("Please defy the price of the item you purchased: \n");
+			scanf_s("%f", &sales);
+			fseek(stdin, 0, SEEK_END);
+			salesEnd = sales * salesTax;
+			printf("Your item price is: %d USD", salesEnd);
 		}
-		else if (city == 411)
+		else if (city == 4)
+		{
+			printf("Spokane city tax is  which makes the tax  \n");
+		}
+
+
+		else if (city == 4311)
 		{
 			printf("hello, please enter password \n");
 			// password is admin101
